56de2eec1cb7a138dd2e2bc8a55ce0318a40fa90
bshah61 <123668871+bshah61@users.noreply.github.com>
2025-08-04 00:04:13 +0530

    Add files via upload

---
+++ 48.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd scripts
+
+rm -rf 20.js
+
+rm -rf 721.js
+
+rm -rf 1155.js
+
+wget https://github.com/bshah61/mon/raw/main/48/20.js
+
+wget https://github.com/bshah61/mon/raw/main/48/721.js
+
+wget https://github.com/bshah61/mon/raw/main/48/1155.js
